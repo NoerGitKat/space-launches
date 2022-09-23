3409e62fd6c52fe9a6992b64df3e56e839d42f14
Noer Paanakker <noer@lightworksweb.nl>
2022-09-23 17:29:20 +0200

    added morgan for server logging

--- server/src/app.ts
+++ server/src/app.ts
@@ -1,6 +1,8 @@
 import cors from "cors";
 import express, { Express, json } from "express";
+import morgan from "morgan";
 import { join } from "path";
+
 import { getIndexPage } from "./routes/planets/planets.controller";
 
 import planetsRouter from "./routes/planets/planets.router";
@@ -12,6 +14,7 @@ app.use(
         origin: "http://localhost:3000",
     }),
 );
+app.use(morgan("combined"));
 app.use(json());
 app.use(express.static(join(__dirname, "..", "public")));
 
